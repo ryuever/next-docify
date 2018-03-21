6681cd8932939a203e94ad4d54bca044949a5910
youchao.liu <liuyouchao111@gmail.com>
2018-03-21 15:57:29 +0800

    fix `markdown` parse `summary` with <p></p> tag

--- components/DocContent.js
+++ components/DocContent.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import markdown from '../docs/Android-SDK/开发指南/创建项目/开发注意事项.md';
+import md from '../lib/markdown-loader/Parser';
 
-const DocContent = () => {
-  // const { postmeta } = props;
-  // const { content } = postmeta;
-  // const html = md.render(content);
-  const { content } = markdown;
+// import markdown from '../docs/Android-SDK/开发指南/创建项目/开发注意事项.md';
+
+const DocContent = (props) => {
+  const { postmeta } = props;
+  const { content } = postmeta;
+  const html = md.render(content);
+  // const { content } = markdown;
 
   return (
     <div
       dangerouslySetInnerHTML={{
-        __html: content
+        __html: html
       }}
     >
 
