527be8ae1ca5c855b44a01382ca6174c1fea6b28
youchao.liu <liuyouchao111@gmail.com>
2018-03-25 14:02:13 +0800

    temp fix export path

--- next2.config.js
+++ next2.config.js
@@ -1,5 +1,7 @@
+// const config = require('./config').default;
+
 module.exports = {
-  assetPrefix: '/out',
+  assetPrefix: process.env.NODE_ENV.startsWith('dev') ? '' : '/out',
   exportPathMap: () => ({
     '/': { page: '/' },
     '/docs/ios-sdk': { page: '/docs/ios-sdk' },
