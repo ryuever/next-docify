45dffbcc4f5671868883ce8fcdacd32290933499
youchao.liu <liuyouchao111@gmail.com>
2018-04-12 22:04:10 +0800

    use rollup to bundle library

---
+++ packages/snaker/scripts/rollup.config.js
@@ -0,0 +1,29 @@
+// rollup.config.js
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import glob from 'glob';
+import { join } from 'path';
+
+const files = glob.sync('**/*.js', { cwd: join(process.cwd(), 'src') });
+
+const buildConfig = (filename, sourceDir, destDir, format) => ({
+  input: `${sourceDir}/${filename}`,
+  output: {
+    file: `${destDir}/${filename}`,
+    format,
+  },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**', // only transpile our source code
+    }),
+  ],
+});
+
+const configs = files.reduce((accum, file) => {
+  accum.push(buildConfig(file, 'src', 'lib', 'cjs'));
+  accum.push(buildConfig(file, 'src', 'es', 'es'));
+  return accum;
+}, []);
+
+export default configs;
