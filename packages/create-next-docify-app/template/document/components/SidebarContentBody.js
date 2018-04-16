f19a7f609876b45d0c9d4266ee833e08998e8df1
youchao.liu <liuyouchao111@gmail.com>
2018-04-17 04:00:17 +0800

    add `go back`

--- packages/create-next-docify-app/template/document/components/SidebarContentBody.js
+++ packages/create-next-docify-app/template/document/components/SidebarContentBody.js
@@ -6,21 +6,63 @@ class SidebarContentBody extends Component {
 
     const { manifest } = props;
     let values = [];
+    this.navStack = [];
     if (manifest && manifest.length > 0) {
       values = manifest[0];
     }
 
     this.state = {
       values,
+      baseDepth: 0,
     };
+    this.navStack.push({
+      values: this.state.values,
+      baseDepth: this.state.baseDepth,
+    });
+  }
+
+  handleGoBack(e) {
+    e.stopPropagation();
+
+    const len = this.navStack.length;
+
+    if (len === 1) return;
+    const current = this.navStack.pop();
+    const next = this.navStack.slice(-1)[0];
+
+    if (next) {
+      this.setState(next);
+    }
+  }
+
+  renderGoBack() {
+    return (
+      <div className="nav-go-back" onClick={this.handleGoBack.bind(this)}>
+        <i className="fas fa-arrow-left" />
+        <style jsx>
+          {`
+            .nav-go-back {
+              padding: 12px;
+              background: #eceff1;
+            }
+
+            i {
+              font-size: 24px;
+              color: #757575;
+            }
+          `}
+        </style>
+      </div>
+    );
   }
 
   renderChildren(children) {
     return children.map(child => {
       const { depth } = child;
       const elements = [];
+      const depthCase = depth - this.state.baseDepth;
 
-      switch (depth) {
+      switch (depthCase) {
         case 2:
           elements.push(this.renderLevel2(child));
           break;
@@ -36,6 +78,7 @@ class SidebarContentBody extends Component {
   renderLevel1(child) {
     if (typeof child === 'object') {
       const { children, title } = child;
+
       return (
         <div className="leve1 sidebar-section-header">
           <h3>{title}</h3>
@@ -66,15 +109,15 @@ class SidebarContentBody extends Component {
 
   renderLevel2(child) {
     const { children, id, title } = child;
-    const className =
-      children.length === 0 ? ' section-page' : ' section-header';
+    const isPage = children.length === 0;
+    const className = isPage ? ' section-page' : ' section-header';
 
     return (
       <div className="level-2" key={id}>
         <a href="#" className={className}>
           {title}
         </a>
-        {children.length > 0 ? this.renderChildren(children) : null}
+        {!isPage ? this.renderChildren(children) : null}
         <style jsx>
           {`
             .section-page {
@@ -105,17 +148,41 @@ class SidebarContentBody extends Component {
     );
   }
 
+  handleRenderNextNav(child, e) {
+    e.stopPropagation();
+    const { depth } = child;
+    const data = {
+      values: child,
+      baseDepth: depth - 1,
+    };
+
+    this.setState(data);
+    this.navStack.push(data);
+  }
+
   renderLevel3(child) {
     const { postmeta } = this.props;
     const { children, id, title } = child;
-    const className =
-      children.length === 0 ? ' section-page' : ' section-header';
+    const isPage = children.length === 0;
+
+    const className = isPage ? ' section-page' : ' section-header';
 
     return (
       <div className="level-3 box" key={id}>
-        <a href="#" className={className}>
-          {title}
-        </a>
+        {isPage && (
+          <a href="#" className={className}>
+            {title}
+          </a>
+        )}
+
+        {!isPage && (
+          <div
+            onClick={this.handleRenderNextNav.bind(this, child)}
+            className={className}
+          >
+            {title}
+          </div>
+        )}
         {children.length > 0 && <i className="fas fa-angle-right" />}
         <style jsx>{`
           .level-3 {
@@ -161,23 +228,11 @@ class SidebarContentBody extends Component {
     if (!manifest || manifest.length === 0) return null;
 
     return (
-      <div className="help-result questionsContainer">
-        <div className="questionsBox">
+      <div>
+        <div>
+          {this.renderGoBack()}
           {this.renderContent(this.state.values)}
         </div>
-        <style jsx>
-          {`
-            .questionsContainer {
-              width: 100%;
-              display: flex;
-              justify-content: center;
-            }
-
-            .questionsBox {
-              width: 1200px;
-            }
-          `}
-        </style>
       </div>
     );
   }
